0fc4ea2479d286789620596e3ac57d3592ef29f6
Darshan Rathod <97824353+rathoddarshan@users.noreply.github.com>
2025-12-02 21:41:57 +0530

    Time: 1 ms (99.27%), Space: 47.1 MB (38.7%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,19 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
-
-        int count = 0;
-        int end = 0; 
-        int far= 0;
+        int end = 0;
+        int jump = 0;
+        int far = 0;
 
         for(int i=0; i<nums.length-1; i++){
             far = Math.max(far, i+nums[i]);
-            if(i == end){
-                count++;
+            if(i==end){
+                jump++;
                 end = far;
             }
+
         }
 
-        return count;
+        return jump;
         
     }
 }
